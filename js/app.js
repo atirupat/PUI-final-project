0213c306185f7c6b3b71288e656fe19d5b3b012b
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-01 18:48:12 -0400

    trying to do a test animation

--- js/app.js
+++ js/app.js
@@ -15,14 +15,26 @@ app.renderer.resize(window.innerWidth,window.innerHeight);
 app.renderer.view.style.position = 'absolute';
 document.body.appendChild(app.view);
 
+app.ticker.add(() => TWEEDLE.Group.shared.update());
+
 // Assets to be loaded
 PIXI.Assets.add('sky-background', './assets/background-2-02.png');
 PIXI.Assets.load('sky-background').then(bkgSetup);
+
+// time animation in miliseconds!
+const time = 10000;
+
+const xTargets = [100, 500, 100, 300, 700, 0];
+const yTargets = [200, 100, 500, 100, 500, 0];
 
 //Create Buggy Sprite
 const buggyTexture = PIXI.Texture.from('./assets/black-buggy.png');
 const buggy = new PIXI.Sprite(buggyTexture);
 app.stage.addChild(buggy);
+
+new TWEEDLE.Tween(buggy).to({ x: xTargets, y: yTargets }, time)
+    .repeat(Infinity)
+    .start();
 
 // Referencing Example Code from: https://pixijs.io/examples/#/demos-advanced/scratchcard.js
 function bkgSetup() {
