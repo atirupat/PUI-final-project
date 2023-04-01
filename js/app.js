6eb6c4ae9c3954e77462c5996d01a41f23b78e36
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-01 17:37:59 -0400

    Successfully setup PIXI application

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-import { Application, Assets, Sprite } from 'pixi.js';
+// Application Setup (code referenced from: https://youtu.be/ajaduDDePIY)
+const Application = PIXI.Application;
 
-// Starter code referenced from: https://www.npmjs.com/package/pixi.js?activeTab=readme
-const app = new Application({width: 256, height: 256});
+const app = new Application(
+  {
+    width: 1000,
+    height: 500,
+    transparent: false,
+    antialias: true
+  }
+);
+
+app.renderer.resize(window.innerWidth,window.innerHeight);
+
+app.renderer.view.style.position = 'absolute';
 document.body.appendChild(app.view);
 
-const texture = await Assets.load('C:\Users\anish\Desktop\PUI 2023\final-project\PUI-final-project\assets\sky background.svg');
+// Assets to be loaded
+PIXI.Assets.add('sky-background', './assets/sky background.svg');
+PIXI.Assets.load('sky-background').then(setup);
 
-const skyBackground = new Sprite(texture);
-
-skyBackground.x = app.renderer.width / 2;
-skyBackground.y = app.renderer.height / 2;
-
-app.stage.addChild(skyBackground);
