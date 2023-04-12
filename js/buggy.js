1fa08d50db24277f06277f688661585df4eb5914
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-12 15:16:45 -0400

    Progress Made?

--- js/buggy.js
+++ js/buggy.js
@@ -1,16 +1,17 @@
 // Global Variables
 
-const roadHeight = document.querySelector('.buggy-road').offsetHeight;
-const roadLength = document.querySelector('.buggy-road').clientWidth;
+const roadHeight = document.querySelector('.buggy-road').clientHeight;
+const roadLength = document.querySelector('.buggy-road').scrollWidth;
 console.log(roadHeight);
 console.log(roadLength);
 
 SVG.on(document, 'DOMContentLoaded', function() {
-  let draw = SVG().addTo('.buggy-road').size(300, 300);
-  let rect = draw.rect(8000, 100).attr({ fill: '#f06' });
+  // Draw Bugg
+  let draw = SVG().addTo('.buggy-road').size(roadLength, roadHeight*2);
+  let rect = draw.rect(roadLength, roadHeight).attr({ fill: '#f06' });
 
   // calculate y position of road
-  const roadPositionY = roadHeight - 40;
+  const roadPositionY = roadHeight/2;
 
   rect.attr({ x: 0, y: roadPositionY})
 })
