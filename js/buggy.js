3a38cfe4972b0441790fec42126706917ac4cc2c
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-12 10:44:54 -0400

    changed background color

--- js/buggy.js
+++ js/buggy.js
@@ -1,6 +1,7 @@
-import { SVG, extend as SVGextend, Element as SVGElement } from '@svgdotjs/svg.js'
+// import { SVG } from '@svgdotjs/svg.js';
 
 SVG.on(document, 'DOMContentLoaded', function() {
   let draw = SVG().addTo('body').size(300, 300);
   let rect = draw.rect(100, 100).attr({ fill: '#f06' });
 })
+
