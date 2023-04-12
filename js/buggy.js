c4772b87027e47fb405c6844c115dc503065816a
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-12 10:59:40 -0400

    Started messing around with SVG.js code

--- js/buggy.js
+++ js/buggy.js
@@ -1,7 +1,17 @@
-// import { SVG } from '@svgdotjs/svg.js';
+// Global Variables
+
+const roadHeight = document.querySelector('.buggy-road').offsetHeight;
+const roadLength = document.querySelector('.buggy-road').offsetWidth;
+console.log(roadHeight);
+console.log(roadLength);
 
 SVG.on(document, 'DOMContentLoaded', function() {
-  let draw = SVG().addTo('body').size(300, 300);
-  let rect = draw.rect(100, 100).attr({ fill: '#f06' });
+  let draw = SVG().addTo('.buggy-road').size(300, 300);
+  let rect = draw.rect(roadLength, 100).attr({ fill: '#f06' });
+
+  // calculate y position of road
+  const roadPositionY = roadHeight - 40;
+
+  rect.attr({ x: 0, y: roadPositionY})
 })
 
