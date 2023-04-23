5ab65933ba39b8e024f5d9b7bad72688bff7345e
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-23 13:30:28 -0400

    Trying to Debug Progress Tracker Code

--- js/buggy.js
+++ js/buggy.js
@@ -1,9 +1,9 @@
 // Global Variables
 
-const roadHeight = document.querySelector('.buggy-road').clientHeight;
-const roadLength = document.querySelector('.buggy-road').scrollWidth;
-console.log(roadHeight);
-console.log(roadLength);
+// const roadHeight = document.querySelector('.buggy-road').clientHeight;
+// const roadLength = document.querySelector('.buggy-road').scrollWidth;
+// console.log(roadHeight);
+// console.log(roadLength);
 
 // SVG.on(document, 'DOMContentLoaded', function() {
 //   let draw = SVG().addTo('.buggy-road').size(roadLength, roadHeight*2);
@@ -20,4 +20,7 @@ console.log(roadLength);
 
 //   rect.attr({ x: 0, y: roadPositionY})
 // })
+
+// Add Progress Tracker
+$('body').progressTracker();
 
