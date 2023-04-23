2be373c3e34bb680f77851c3afb43ae4b20db420
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-23 12:11:59 -0400

    Added JQuery Progress Tracker Plug-in

--- js/buggy.js
+++ js/buggy.js
@@ -5,20 +5,19 @@ const roadLength = document.querySelector('.buggy-road').scrollWidth;
 console.log(roadHeight);
 console.log(roadLength);
 
-SVG.on(document, 'DOMContentLoaded', function() {
-  // let timeline = 
-  let draw = SVG().addTo('.buggy-road').size(roadLength, roadHeight*2);
-  // Draw Buggy
-  let buggyImage = draw.image('./assets/black-buggy.png');
-  buggyImage.size(200,200);
-  buggyImage.attr({x:0, y:0});
+// SVG.on(document, 'DOMContentLoaded', function() {
+//   let draw = SVG().addTo('.buggy-road').size(roadLength, roadHeight*2);
+//   // Draw Buggy
+//   let buggyImage = draw.image('./assets/black-buggy.png');
+//   buggyImage.size(200,200);
+//   buggyImage.attr({x:0, y:0});
 
-  // Draw Road
-  let rect = draw.rect(roadLength + 30, roadHeight).attr({ fill: '#f06' });
+//   // Draw Road
+//   let rect = draw.rect(roadLength + 30, roadHeight).attr({ fill: '#f06' });
 
-  // calculate y position of road
-  const roadPositionY = 122;
+//   // calculate y position of road
+//   const roadPositionY = 122;
 
-  rect.attr({ x: 0, y: roadPositionY})
-})
+//   rect.attr({ x: 0, y: roadPositionY})
+// })
 
