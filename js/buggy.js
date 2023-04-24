b53ca7d51332f74a47f3a56ce4008df2e6a9a2ef
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-24 19:18:51 -0400

    Fixed Buggy Overlay in Main Page

--- js/buggy.js
+++ js/buggy.js
@@ -1,9 +1,9 @@
 // Global Variables
 
-// const roadHeight = document.querySelector('.buggy-road').clientHeight;
-// const roadLength = document.querySelector('.buggy-road').scrollWidth;
-// console.log(roadHeight);
-// console.log(roadLength);
+const roadHeight = document.querySelector('.buggy-road').clientHeight;
+const roadLength = document.querySelector('.buggy-road').scrollWidth;
+console.log(roadHeight);
+console.log(roadLength);
 
 // SVG.on(document, 'DOMContentLoaded', function() {
 //   let draw = SVG().addTo('.buggy-road').size(roadLength, roadHeight*2);
