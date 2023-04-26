10bffd7a0d85be073c00b346fa1f06dc93a9da27
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-26 16:10:10 -0400

    Enter button interaction working

--- js/navigation.js
+++ js/navigation.js
@@ -1,41 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() { 
 
-  // Global Variables
-//   const goButton = document.querySelector('#go-button');
-//   const checkpointMarker = document.querySelector('.checkpoint-marker');
+    // Allow Users to navigate to buggy.html when pressing the "Enter" button
 
-  // Get each Checkpoint Marker
-//   const markerOne = document.getElementById('marker-one');
-//   const markerTwo = document.querySelector('#marker-two');
-//   const markerThree = document.querySelector('#marker-three');
-//   const markerFour = document.querySelector('#marker-four');
-//   const markerFive = document.querySelector('#marker-five');
-
-//   console.log(markerOne);
-  // Create and Display Overlays when Hovering over Checkpoints
-  // markerOne.addEventListener('mouseover', createOverlay, false);
-
-  // function createOverlay () {
-  //   console.log('function entered');
-  //   const template = document.querySelector('.nav-overlay');
-  //   const clone = template.content.cloneNode(true);
-
-  //   element = clone.querySelector('.map-overlay');
-  //   console.log(element);
-
-  //   const mapView = document.querySelector('#map-view');
-  //   mapView.prepend(element);
-
-  //   updateOverlay(element);
-  // }
-
-  // function updateOverlay(overlayInstance) {
-  //   const overlayTitle = document.querySelector('.map-hover-title');
-  //   const overlayText = document.querySelector('.map-hover-txt');
-  
-  //   overlayTitle.innerText = '"Front Hills"';
-  //   overlayText.innerText = 'Click to learn about Buggy and the Race';
-  // }
+    $(document).keyup(function(event) {
+      if (event.which === 13) {
+          window.location.href = 'buggy.html';
+      }
+    });
 
     //Using JQuery API to Display Informative Overlays when Hovering Over Checkpoint Markers 
     $('#marker-one').hover(
