6c5cfb0d78dffb22206b66de263169c54596df66
atirupat <122955023+atirupat@users.noreply.github.com>
2023-04-30 15:39:43 -0400

    Implemented Section Header Updating

--- js/buggy.js
+++ js/buggy.js
@@ -6,55 +6,17 @@ let locationTxt = document.querySelector('#display-location');
 // Initial left position of '.times-by-division' div
 const initialPosition = $('.buggy-road').position();
 
-document.addEventListener("DOMContentLoaded", function() { 
-  console.log(initialPosition);
-
-  //Use JQuery to Dynamically Change Header Location Text
-  // Function to check if something is in viewport referenced from: https://stackoverflow.com/questions/20791374/jquery-check-if-element-is-visible-in-viewport
-  $.fn.isInViewport = function() {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
-
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-  }
-
-  // $(window).on('scroll', function () {
-  //   if ($('#front-hills').isInViewport()) {
-  //     locationTxtContents = 'Front Hills';
-
-  //     locationTxt.innerHTML = locationTxtContents;
-  //     console.log(locationTxt.innerHTML);
-  //   } 
-    
-  //   if ($('#free-roll').isInViewport()) {
-  //     console.log('entered free roll');
-  //     locationTxtContents = 'Free Roll';
-
-  //     locationTxt.innerHTML = locationTxtContents;
-  //     console.log(locationTxt.innerHTML);
-  //   }
-
-  // });
-
-  console.log(locationTxtContents);
-  
-
-  console.log(locationTxt.innerHTML);
-});
-
 // -----------------------------------------------------------------------------------------------------------------------------------------------
-// Update Width of Progress Bar
+// Update Header While Scrolling
+
 $(window).on('scroll', function () {
+  // Update Width of Progress Bar
   let currentPosition = $('.buggy-road').offset();
   const documentWidth = $(document).width();
 
 
   // Subtract length of buggy road from Document Width
   const buggyRoadWidth = $('.buggy-road').width();
-
   let newDocumentWidth = documentWidth - buggyRoadWidth;
 
   // Convert Position of Buggy Road into a Percentage
@@ -62,6 +24,47 @@ $(window).on('scroll', function () {
 
   // Change Progress Bar Width in CSS
   $('.progress-bar').css({width: percentCompleted});
+
+  // ----------------------------------------------------------------------------------------------------------------------------------------------
+  // Update Location Text
+
+  // Get Position of Each Section within the Document
+  let newPercentCompleted = (scale(currentPosition.left, initialPosition.left, newDocumentWidth, 0, 100)).toFixed(2);
+
+  // Free Roll
+  const freeRollPosition = $('#free-roll').position();
+  const mapFreeRollPosition = (scale(freeRollPosition.left, initialPosition.left, newDocumentWidth, 0, 100)).toFixed(2);
+  console.log('Free Roll: ' + mapFreeRollPosition);
+
+  // Schenley
+  const schenleyPosition = $('#schenley').position();
+  const mapSchenleyPosition = (scale(schenleyPosition.left, initialPosition.left, newDocumentWidth, 0, 100)).toFixed(2);
+  console.log('Schenley: ' + mapSchenleyPosition);
+
+  //The Chute
+  const chutePosition = $('#chute').position();
+  const mapChutePosition = (scale(chutePosition.left, initialPosition.left, newDocumentWidth, 0, 100)).toFixed(2);
+  console.log('Chute: ' + mapChutePosition);
+
+  // Back Hills
+  const backHillsPosition = $('#back-hills').position();
+  const mapBackHillsPosition = (scale(backHillsPosition.left, initialPosition.left, newDocumentWidth, 0, 100)).toFixed(2);
+  console.log('Back Hills: ' + mapBackHillsPosition);
+
+  // Change Header Text Depending on which Section the User is Currently In
+  if (newPercentCompleted >= 0.00 && newPercentCompleted < mapFreeRollPosition) {
+    locationTxt.innerHTML = 'Front Hills';
+  } else if (newPercentCompleted >= mapFreeRollPosition && newPercentCompleted < mapSchenleyPosition) {
+    locationTxt.innerHTML = 'Free Roll';
+  } else if (newPercentCompleted >= mapSchenleyPosition && newPercentCompleted < mapChutePosition) {
+    locationTxt.innerHTML = 'Schenley Dr.';
+  } else if (newPercentCompleted >= mapChutePosition && newPercentCompleted< mapBackHillsPosition) {
+    locationTxt.innerHTML = '"The Chute"';
+  } else if (newPercentCompleted >= mapBackHillsPosition) {
+    locationTxt.innerHTML = 'Back Hills';
+  };
+
+  console.log(newPercentCompleted);
 });
 
 // Mapping Numbers function sourced from: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
